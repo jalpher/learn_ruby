9db202f00595f1f28d14aa01fc09b3d9c14125a6
jalpher <ffej_github@alpher.com>
2017-09-26 10:55:27 -0400

    all up

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,28 @@
 class Timer
-  #write your code here
+
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds=(secs)
+    @seconds = secs
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    minutes, seconds = @seconds.divmod(60)
+    hours, minutes = minutes.divmod(60)
+
+    sec_string = "%02d" % seconds
+    min_string = "%02d" % minutes
+    hr_string = "%02d" % hours
+
+    ts = hr_string + ':' + min_string + ':' + sec_string
+  end
 end
+
+
+# result_string = "%02d" % some_number # => "03"
