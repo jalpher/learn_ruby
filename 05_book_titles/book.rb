6660d997d52d787379e28721e97e7187e2777be3
jalpher <ffej_github@alpher.com>
2017-09-26 10:28:54 -0400

    titles works

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+
+  def title=(name)
+    @title = make_title(name)
+  end
+
+  def title
+    @title
+  end
+
+
+  def make_title(phrase)
+    exceptions = ["and", "a", "an", "in", "of", "the"]
+    space = " "
+    result = ""
+    words = phrase.split(" ")
+    words.each_with_index do |w, n|
+      unless exceptions.include?(w) and n!=0
+        w.capitalize!
+      end
+      result = result + w + space
+    end
+    return result.strip!
+  end
 end
